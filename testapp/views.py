91d0874a3c2fc367054e776fa98d07ccecc3c19f
grmartinezme.ext@acciona.com <grmartinezme.ext@acciona.com>
2023-09-25 17:57:55 +0200

    secret_key en .ignore

--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
 
 #get
 def get_example(request):
@@ -21,3 +22,10 @@ def post_example(request):
                 'headers': dict(request.headers),
                 'body': request.body.decode()}}
     return JsonResponse(response, safe=False, json_dumps_params={'indent': 2})
+
+def gallery(request):
+    return render(request, 'testapp/gallery.html')
+
+def gallery_photo(request, photo):
+    context = {'photo': photo}
+    return render(request, 'testapp/gallery_photo.html', context)
--- webproject/settings.py
+++ webproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l&sjx!a1v4iwn7nv=jv76w0!u$*n4d-5)$9j7&y8vkx_(sia@4'
+SECRET_KEY = open(BASE_DIR / '.secret_key', 'r', encoding='utf-8').read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
